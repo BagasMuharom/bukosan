1f4147ad192b4aac48d229502a098031aae31d54
BagasMuharom <bagashidayat45@gmail.com>
2017-04-21 11:12:49 +0700

    Mulai membuat tampilan

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,29 @@
 
 use Illuminate\Http\Request;
 
-Route::get('/', function (){
-    return view('public.home')->with([
-        "title" => "Beranda",
-        "content" => "Ini beranda !"
-    ]);
-});
+Route::get('/', 'HomePageController@HomePage');
+
 Route::get('/cari/{lokasi}',function ($lokasi){
     return 'Anda mencari kosan di daerah : ' . $lokasi;
 });
+
 Route::get('req',function (){
     return view('req');
 });
+
 Route::post('req',function (Request $request){
     return $request->nama;
 });
 
-Auth::routes();
+Route::get('masuk','Auth\LoginController@LoginPage')->name('login');
+Route::post('masuk','Auth\LoginController@Process');
+
+Route::get('daftar','Auth\RegisterController@RegisterPage')->name('register');
+
+//Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('input', function(){
+    return view('test.input');
+});
